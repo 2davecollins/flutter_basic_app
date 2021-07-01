37ce024e954ff4dd1654c20503d47dc313749bc1
David <2davecollins@gmail.com>
2021-07-01 10:33:24 +0100

    init state

---
+++ lib/AboutPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Center(
+        child: Container(
+          height: 80,
+          width: 150,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+          child: FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'About Us',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
